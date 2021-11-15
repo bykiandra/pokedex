52f6dc090a659a0c6a7b14da8aa8e6dcb9406196
Kiandra Ginevra <kiandrajayne@gmail.com>
2021-11-15 14:32:06 +1100

    Add Tailwind CSS.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
 import pokeball from './img/pokeball.svg';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <img src={pokeball} width="200px" />
+      <img src={pokeball} className="logo mx-auto my-10 w-48" />
+
+      <div className="container max-w-screen-sm mx-auto p-4 bg-white rounded-xl">
+        Hello world
+      </div>
     </div>
   );
 }
