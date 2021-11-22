675c18aae63c26361fba2da8982a7491b4643810
Kiandra Ginevra <kiandrajayne@gmail.com>
2021-11-22 15:08:25 +1100

    Add name format for alternate forms.

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -4,13 +4,35 @@ import Info from './Info';
 
 const Pokemon = (pokemon) => {
   const p = pokemon.pokemon;
+
+  const nameUpperCase = (name) => {
+    return name.charAt(0).toUpperCase() + name.slice(1)
+  }
+
+  const pokemonName = (name) => {
+    let nameSplit = []
+    let displayName = ''
+
+    if (name.includes('-')) {
+      nameSplit = name.split('-')
+      nameSplit[0] = nameUpperCase(nameSplit[0])
+      nameSplit[1] = ' (' + nameUpperCase(nameSplit[1]) + ' Form)'
+
+      displayName = nameSplit.join('')
+    } else {
+      displayName = name.charAt(0).toUpperCase() + name.slice(1)
+    }
+
+    return displayName
+  }
+
   return (
     <div className="mt-6">
       <h1 className="border-b border-solid text-lg mb-2">
         <img src={p.sprites.versions['generation-viii'].icons.front_default}
           className="float-right -mt-9"
           alt="Pokemon sprite" />
-        {p.name.charAt(0).toUpperCase() + p.name.slice(1)}
+        {pokemonName(p.name)}
       </h1>
       <div className="grid grid-cols-2 gap-3">
         <img src={p.sprites.other['official-artwork'].front_default}
