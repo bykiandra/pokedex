a66875ea773e61815abb0eaff976370a5829dc3f
Kiandra Ginevra <kiandrajayne@gmail.com>
2021-11-22 16:13:19 +1100

    Add styling to stat bars.

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import './Stats.css'
+
 const Stats = (stats) => {
   return (
-    <div className="grid grid-cols-2 gap-1">
-      HP: <progress max="255" value={stats.hp} /><br />
-      Attack: <progress max="255" value={stats.att} /><br />
-      Defense: <progress max="255" value={stats.def} /><br />
-      Sp.Attack: <progress max="255" value={stats.spa} /><br />
-      Sp.Defense: <progress max="255" value={stats.spd} /><br />
-      Speed: <progress max="255" value={stats.spe} />
+    <div className="grid grid-cols-2">
+      HP: <progress max="255" value={stats.hp} className="hp" />
+      Attack: <progress max="255" value={stats.att} className="att" />
+      Defense: <progress max="255" value={stats.def} className="def" />
+      Sp.Attack: <progress max="255" value={stats.spa} className="spa" />
+      Sp.Defense: <progress max="255" value={stats.spd} className="spd" />
+      Speed: <progress max="255" value={stats.spe} className="spe" />
     </div>
   );
 }
