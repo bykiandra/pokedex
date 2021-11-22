a1345c3fae352da58cf6102cd32e1a3b31296229
Kiandra Ginevra <kiandrajayne@gmail.com>
2021-11-22 16:28:59 +1100

    Update footer component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="container max-w-screen-sm mx-auto p-4 bg-white rounded-xl text-xs text-center my-4">
-      Best viewed in Firefox.<br />
+    <footer className="container max-w-screen-sm mx-auto p-4 bg-white rounded-xl text-xs text-center my-4">
       View the code on&nbsp;
       <a href="https://github.com/crownedfoxes/pokedex" target="_blank"
         className="text-red-500 hover:text-red-600" rel="noreferrer" >
         GitHub
       </a>.
-    </div>
+    </footer>
   );
 }
 
